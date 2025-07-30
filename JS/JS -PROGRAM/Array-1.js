91bb2ac0c1e6ede849b8f8b7a0cff220b2496e1a
Priyanshu Kumar <97750379+Priyanshu23565@users.noreply.github.com>
2025-07-30 17:59:46 +0530

    Create Array-1.js

---
+++ JS/JS -PROGRAM/Array-1.js
@@ -0,0 +1,10 @@
+let colors = ["red", "green", "blue"];
+
+console.log(colors.length);   
+console.log(colors[1]);        
+
+colors.push("yellow");
+console.log(colors);           
+
+colors.pop();
+console.log(colors);       
